7464ab4870cb8f13e6a2ef1bd36452c1c08e5242
sevitha priya <60482122+sevitha7777@users.noreply.github.com>
2022-10-21 00:33:07 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class sum {
+public static void main(String[] args)
+	{
+		 try (Scanner s = new Scanner(System.in)) {
+			int n =s.nextInt();
+			System.out.println("Number:"+n);
+		int sum = 0;
+
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+			
+		System.out.println("Sum:"+sum);
+		}
+
+	}
+}
+
+
